bd050477206ea10bef6a1fc07f066646b95dac02
ong-king <1318281415@qq.com>
2018-06-09 14:52:53 +0800

    改地址

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Axios from 'axios'
 
 let axiosConfig = Axios.create({
-    baseURL: 'http://192.168.20.3:8080/',   //后台接口的公用地址
+    baseURL: 'http://114.116.25.220:8081',   //后台接口的公用地址
     timeout: '10000',   //所有请求10s后过期
     withCredentials: true,    //是否允许跨域
 });
